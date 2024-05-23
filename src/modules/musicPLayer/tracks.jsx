7b5d59eec9d80945e55ab280e4857a7875a2d947
Stimur2304 <samatov.timur33@gmail.com>
2024-05-23 14:46:28 +0600

    player

---
+++ src/modules/musicPLayer/tracks.jsx
@@ -0,0 +1,19 @@
+import killer from "../../consants/audio/killer.mp3"
+import maneskin from "../../consants/audio/maneskin.mp3"
+const tracks = [
+    {
+      title: 'Track 1',
+      src: killer
+    },
+    {
+      title: 'Track 2',
+      src: maneskin
+    },
+    {
+      title: 'Track 3',
+      src: '/music/track3.mp3'
+    }
+  ];
+  
+  export default tracks;
+  
